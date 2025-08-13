554c3987f056874b6e369c475e0daf198ab33f6d
shiva kanojia <119107901+shiva0047@users.noreply.github.com>
2025-08-13 16:24:41 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://gym-fitness-website.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   },
